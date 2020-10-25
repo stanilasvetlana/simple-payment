71e140708a4e1106f991c70db951c5db5b8404df
Svetlana <stanilasvetlana@yahoo.com>
2020-10-25 15:28:01 +0200

    styling, validation added

--- src/app/views/payment/payment.component.ts
+++ src/app/views/payment/payment.component.ts
@@ -1,18 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PaymentService} from '../../services/payment.service';
 import {PaymentStatus} from '../../services/payment.models';
-import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder,  FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {tap} from 'rxjs/operators';
 import {ToastrService} from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.css']
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit{
   public form!: FormGroup;
+
 
   constructor(
     private paymentService: PaymentService,
@@ -26,10 +28,10 @@ export class PaymentComponent {
   private createForm(): void {
     this.form = this.formBuilder.group({
       creditCardNumber: [undefined, [Validators.required, Validators.pattern(regexValidator)]],
-      cardholder: [undefined],
-      expirationDate: [undefined],
-      securityCode: [undefined],
-      amount: [undefined]
+      cardholder: [undefined, Validators.required],
+      expirationDate: [undefined, Validators.required],
+      securityCode: [undefined, [Validators.minLength(3), Validators.maxLength(3) ]],
+      amount: [undefined, [Validators.required, Validators.min(0)]]
     });
   }
 
@@ -52,16 +54,6 @@ export class PaymentComponent {
       })
     ).subscribe();
   }
-
-  shouldDisplayErrorForControl(controlName: string): boolean {
-    const control: AbstractControl | null = this.form.get(controlName);
-
-    if (!control) {
-      throw Error('Invalid control name');
-    }
-    return control.dirty && control.invalid || control.touched;
-  }
 }
-
 
 const regexValidator = `^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}(?:2131|1800|35\\d{3})\\d{11})$`;
